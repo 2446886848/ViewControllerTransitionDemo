f00e1de0e12fb62fdb0b9b66b66957471727a8c1
wuzhihe <2446886848@qq.com>
2016-02-19 09:51:38 +0800

    Initial Commit

---
+++ ViewControllerTransitionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ViewControllerTransitionDemo
+//
+//  Created by 吴志和 on 16/2/19.
+//  Copyright © 2016年 wuzhihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
